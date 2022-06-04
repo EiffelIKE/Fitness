94d98793c3ff04c1b8511e3672da4177c728f1bd
EiffelIKE <jctrujillo759@gmail.com>
2022-06-04 15:53:51 -0500

    First commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,8 @@ import { useNavigate } from 'react-router-dom';
 
 const Card = ({ title, description,img, leftColor, rightColor})=>{
   const navigate=useNavigate()
-let handleClick=()=>navigate('./view')
+let handleClick=()=>
+navigate('./view')
   return(
   <>
     <div  className='card '
